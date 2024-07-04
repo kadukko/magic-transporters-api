884270d4fa47c26a4b60eaf838aec21521f1adce
Ricardo <ricardosouza_defn@outlook.com>
2024-07-03 23:18:14 -0300

    Add swagger api documentation

--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,15 @@
 import express from 'express'
+import routes from './routes'
+import swaggerUI from 'swagger-ui-express'
+import path from 'node:path'
+import YAML from 'yamljs'
 
 const app = express()
 app.use(express.json())
+
+routes.applyMiddleware(app)
+
+const swaggerFilename = path.join(__dirname, '../swagger.yaml')
+app.use('/', swaggerUI.serve, swaggerUI.setup(YAML.load(swaggerFilename)))
 
 export default app
